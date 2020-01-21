351a7247a000d5391cef4aeae0648af08b4b2990
sandwich189 <46155245+sandwich189@users.noreply.github.com>
2020-01-21 21:28:23 +0800

    added jin's enemy AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Transform playerPos;
+
+    void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+
+}
