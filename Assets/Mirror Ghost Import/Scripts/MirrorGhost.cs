90ae8302e3e1a805be8256b8a628cbc344aa2951
shijie-l <59986979+shijie-l@users.noreply.github.com>
2020-02-01 17:25:56 +0800

    Added basic mirror ghost codes & countdown timer
    Added prefabs, scripts, unity sprites and a prototype scene for merging with the master branch in main repository.

---
+++ Assets/Mirror Ghost Import/Scripts/MirrorGhost.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorGhost : MonoBehaviour
+{
+    [SerializeField] Player player;
+    [SerializeField] EntangleFadeInOut entangleImage;
+
+    private bool debugMsg = false;
+    private bool startEntanglement = false;
+    private bool entanglementInitialised = false;
+    public bool mirrorGhostKillsPlayer = false;
+    public bool isCoveredByCloth = false;
+
+    public Transform image;
+    private RectTransform imageRectTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(player.trappedByMirrorGhost == true && isCoveredByCloth == false)
+        {
+            if (startEntanglement == false)
+            {
+                startEntanglement = true;
+                InitialiseEntangle();
+            }
+        }
+        
+        if(entanglementInitialised == true && isCoveredByCloth == true)
+        {
+            Debug.Log(transform.name + " sequence deinitiated.");
+            entanglementInitialised = false;
+            entangleImage.startFadeOut = true;
+            entangleImage.startFadeInToggle = false;
+            player.trappedByMirrorGhost = false;
+            player.pauseOnTriggerStay = false;
+            startEntanglement = false;
+            entanglementInitialised = false;
+        }
+        else if(isCoveredByCloth == true && entanglementInitialised == false && player.trappedByMirrorGhost == false)
+        {
+            if(debugMsg == true)
+            {
+                debugMsg = false;
+                Debug.Log("Player has clothed the mirror without triggering the ghost.");
+            }
+        }
+    }
+
+    void InitialiseEntangle()
+    {
+        entanglementInitialised = true;
+
+        entangleImage.ToggleImageEnable();
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isCoveredByCloth == false)
+            {
+                Debug.Log("Cloth has been placed onto the mirrorfront.");
+                debugMsg = true;
+                isCoveredByCloth = true;
+            }
+            else if(isCoveredByCloth == true)
+            {
+                Debug.Log("Cloth has been removed from the mirrorfront.");
+                debugMsg = true;
+                isCoveredByCloth = false;
+            }
+        }
+
+    }
+}
---
+++ Assets/Mirror Ghost Import/Scripts/Player.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] MirrorGhost mirror;
+
+    private bool deathDebug = false;
+
+    private Rigidbody2D rBody;
+    private bool facingRight = true;
+    public bool trappedByMirrorGhost = false;
+    public bool collisionTrigger = false;
+    public bool isPlayerDead = false;
+    public bool pauseOnTriggerStay = false;
+
+    public Transform dragPlayerToTarget;
+    private Vector2 targetPosition;
+    public float dragSpeed = 1f;
+    private float dragStep = 0f;
+
+    public float moveSpeed = 5f;
+    float xMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        targetPosition = new Vector2(dragPlayerToTarget.position.x, transform.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        if(trappedByMirrorGhost == true)
+        {
+            MovementManager(true);
+        }
+        else if(isPlayerDead == true)
+        {
+            MovementManager(true);
+        }
+        else
+        {
+            MovementManager(false);
+        }
+    }
+
+    private void Update()
+    {
+        //checking player death (debug)
+        if(isPlayerDead == true)
+        {
+            if(deathDebug == false)
+            {
+                deathDebug = true;
+                Debug.Log("Player is Dead");
+            }
+        }
+
+        //simulating the mirror dragging the player in LIVE (disabled movement when trapped, but can be negated by red cloth)
+        if(trappedByMirrorGhost == true && mirror.isCoveredByCloth == false)
+        {
+            dragStep = dragSpeed * Time.deltaTime;
+            if(transform.position.x != targetPosition.x)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, dragStep);
+            }
+            else
+            {
+                
+            }
+        }
+
+        
+    }
+
+    private void MovementManager(bool isMovementStopped)
+    {
+        if(isMovementStopped == false)
+        {
+            xMove = Input.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            xMove = 0;
+        }
+
+        rBody.velocity = new Vector2(xMove * moveSpeed, rBody.velocity.y);
+
+        if (xMove > 0 && !facingRight)
+        {
+            FlipPlayerSprite();
+        }
+        else if (xMove < 0 && facingRight)
+        {
+            FlipPlayerSprite();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(mirror.isCoveredByCloth == false && collision.gameObject.name == "MirrorGhost")
+        {
+            Debug.Log(collision.gameObject.name + " sequence initiated.");
+            trappedByMirrorGhost = true;
+        }
+        else if(mirror.isCoveredByCloth == true && collision.gameObject.name == "MirrorGhost")
+        {
+            Debug.Log("Due to the cloth, " + collision.gameObject.name + "'s sequence was not initiated.");
+        }
+        pauseOnTriggerStay = false;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (mirror.isCoveredByCloth == false && collision.gameObject.name == "MirrorGhost")
+        {
+            if(pauseOnTriggerStay == false)
+            {
+                Debug.Log(collision.gameObject.name + " sequence initiated.");
+                trappedByMirrorGhost = true;
+                pauseOnTriggerStay = true;
+            }
+        }
+    }
+    
+    void FlipPlayerSprite()
+    {
+        //turn left into right, or right into left
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 newLocalScale = transform.localScale;
+        newLocalScale.x = newLocalScale.x * -1;
+        transform.localScale = newLocalScale;
+    }
+
+}
