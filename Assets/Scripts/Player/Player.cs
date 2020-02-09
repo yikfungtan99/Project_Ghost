25e49fb261cfdb859fba3bed7ae29bf1fcc4271e
jinzhihoi <56183521+jinzhihoi@users.noreply.github.com>
2020-02-10 01:48:51 +0800

    Added A Separate Script for when ghost collide with player and talisman mechanic

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     public GameObject gm;
     public Inventory iv;
     public bool hidden;
-    public int talisman=3;
+    
     //Player bool
     public bool inventoryOn = false;
     public bool targetOnInteractable = false;
@@ -35,46 +35,5 @@ public class Player : MonoBehaviour
 
     }//End Update
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-
-        if (hidden == false)
-        {
-
-
-           
-            if (other.CompareTag("Enemy"))
-            {
-                if(talisman>0)
-                {
-                    
-                    if(GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled==true)
-                    {
-                        GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled = false;
-                        StartCoroutine(EnemyWake());
-                        talisman--;
-                    }
-                    Debug.Log(talisman);
-                }
-                
-
-
-            }
-
-
-        }
-
-
-
-
-    }
-    IEnumerator EnemyWake()
-    {
-        //This is a coroutine
-       
-
-        yield return new WaitForSeconds(10);    //Wait one frame
-        GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled = true;
-
-    }
+    
 }
