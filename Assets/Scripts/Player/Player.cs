f7141031b9d3b74a2c02a1befcee277991848aa5
sandwich189 <46155245+sandwich189@users.noreply.github.com>
2020-02-10 18:39:30 +0800

    Revert "Merge pull request #8 from yikfungtan99/Jin-Zhi"
    This reverts commit 5b9390209b7b67126b6257bd943e0eef71c4ed5d, reversing
    changes made to 0cc349bb155f7f41669772d985a2b37f6481e336.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     public GameObject gm;
     public Inventory iv;
     public bool hidden;
+    public int talisman=3;
+
+    //Player float 
+    public float delayTimer;
 
     //Player bool
     public bool inventoryOn = false;
@@ -34,5 +38,46 @@ public class Player : MonoBehaviour
 
     }//End Update
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
 
+        if (hidden == false)
+        {
+
+            if (other.CompareTag("Enemy"))
+            {
+                if(talisman>0)
+                {
+                    
+                    if(GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled==true)
+                    {
+                        GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled = false;
+                        StartCoroutine(EnemyWake());
+                        talisman--;
+                    }
+                    Debug.Log(talisman);
+                }
+                
+
+
+            }
+
+
+        }
+
+    }
+    IEnumerator EnemyWake()
+    {
+        //This is a coroutine
+        yield return new WaitForSeconds(10);    //Wait one frame
+        GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled = true;
+
+    }
+
+    public void ObtainLighter()
+    {
+        //Lighter
+        transform.GetChild(2).GetChild(2).gameObject.SetActive(true);
+        transform.GetChild(3).gameObject.SetActive(true);
+    }
 }
