2077de956b1cab1eb680f2bb8255d25f54febaa5
shijie-l <59986979+shijie-l@users.noreply.github.com>
2020-02-11 22:00:49 +0800

    Added Audio Manager
    Capable of adding audio files and any function can call the PlayAudio() function to produce sounds. Still have to patch some issues.
    
    DO NOT BRANCH FROM THIS

---
+++ Assets/Scripts/Audio/Audio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Audio
+{
+    //! Name allocation for the audioclip being stored
+    public string name;
+
+    //! Storage for an audioclip
+    public AudioClip audioClip;
+
+    //! Modules that adjust the audio clip variables
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+}
---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Audio[] audioList;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        //! singleton design pattern to avoid duplicate audio managers
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        //! Retain audio manager upon loading new scene
+        DontDestroyOnLoad(gameObject);
+
+        //! Assign audio variables to a new AudioSource component for each registered audioclip
+        foreach (Audio a in audioList)
+        {
+            a.audioSource = gameObject.AddComponent<AudioSource>();
+
+            a.audioSource.clip = a.audioClip;
+            a.audioSource.volume = a.volume;
+            a.audioSource.pitch = a.pitch;
+            a.audioSource.loop = a.loop;
+        }
+    }
+
+    //! Only use this when an audioclip is to be played at the start of game (i.e. BGM theme)
+    void Start()
+    {
+        PlayAudio("Test Theme");
+    }
+
+    public void PlayAudio(string searchName)
+    {
+        //! Searching for equivalent audio name to identify and play
+        Audio a = Array.Find(audioList, audio => audio.name == searchName);
+
+        //! In case audio name cannot be identified
+        if(a == null)
+        {
+            Debug.LogWarning("Audio: " + searchName + " is not found!");
+            return;
+        }
+
+        a.audioSource.Play();
+    }
+}
