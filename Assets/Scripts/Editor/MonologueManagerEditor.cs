5283c2808e39f3f230fb2f9f5646baa0c60c3485
yikfungtan99 <50047634+yikfungtan99@users.noreply.github.com>
2020-02-17 21:09:28 +0800

    aaa

---
+++ Assets/Scripts/Editor/MonologueManagerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MonologueManager))]
+public class MonologueManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MonologueManager mm = (MonologueManager)target;
+
+        if(GUILayout.Button("Perform Debug"))
+        {
+            mm.DebugFunction();
+        }
+    }
+}
---
+++ Assets/Scripts/Sentences.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sentences
+{
+    //! Brief description of what the sentence says or when it should be shown
+    [Tooltip("Brief description of what the sentence implies or when it should be shown.")]
+    public string description;
+
+    //! Index for sentence
+    [Tooltip("Sentence with the respective index will be shown in Monologue when called by a function.")]
+    public int index;
+
+    //! What will actually show in the Monologue text in game
+    [Tooltip("Will be used to display Monoglogue text in the game.")]
+    [TextArea(0, 3)]
+    public string sentenceText;
+
+    //! cooldown time for text to be called it (in the case that it is spammed)
+    [Tooltip("Cooldown before the same line of Monologue text is displayed twice in a row.")]
+    public float cooldown = 3f;
+    public float cooldownCounter;
+}
