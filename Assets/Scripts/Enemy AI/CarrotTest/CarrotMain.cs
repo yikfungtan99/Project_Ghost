0581094a6da57330fade9325c34ef0e48faaee30
sandwich189 <46155245+sandwich189@users.noreply.github.com>
2020-03-01 23:24:00 +0800

    added jin karot 2

---
+++ Assets/Scripts/Enemy AI/CarrotTest/CarrotMain.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotMain : MonoBehaviour
+{
+    
+    public int TalismanStunTime;
+    public Transform[] moveSpots;
+    public Transform[] Set1;
+    public float detectRange;
+    public float stopRange;
+    public float LightSeekingRange;
+    public LayerMask Layer;
+    public LayerMask LightLayer;
+    public LayerMask NoRayLayer;
+    public Animator anima;
+    public GameObject player;
+    public Transform CurrentLight;
+    RaycastHit2D hitInfo;
+    RaycastHit2D LightSeeker;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anima.GetBool("isIdle") == true)
+        {
+            //hitInfo = Physics2D.BoxCast(transform.position, new Vector2(6,1)*2, 0f,transform.right, Layer);
+            hitInfo = Physics2D.Raycast(transform.position, transform.right, detectRange, Layer);
+            LightSeeker = Physics2D.Raycast(transform.position, transform.right, 0, NoRayLayer);
+        }
+        else
+        {
+            //hitInfo = Physics2D.BoxCast(transform.position, new Vector2(1, 1) * 2, 0f, transform.right, Layer);
+            hitInfo = Physics2D.Raycast(transform.position, transform.right, detectRange, Layer);
+            LightSeeker = Physics2D.Raycast(transform.position + new Vector3(0, 1f,0f), transform.right, LightSeekingRange, LightLayer);
+            
+        }
+        
+
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+
+            if (hitInfo.collider.CompareTag("Player") && hitInfo.collider.gameObject.GetComponent<Player>().hidden == false)
+            {
+                anima.SetBool("isChase", true);
+                anima.SetBool("isPatrol", false);
+                anima.SetBool("isLight", false);
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+            }
+            else
+            {
+                anima.SetBool("isChase", false);
+                anima.SetBool("isPatrol", true);
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+        }
+        else if (anima.GetBool("isIdle") == false)
+        {
+            anima.SetBool("isChase", false);
+            anima.SetBool("isPatrol", true);
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            Debug.DrawLine(transform.position, transform.position + transform.right * detectRange, Color.green);
+            
+
+        }
+        if(LightSeeker.collider!=null )
+        {
+            Debug.DrawLine(transform.position + new Vector3(0, 1f, 0f), LightSeeker.point, Color.magenta);
+            if (anima.GetBool("isPatrol") == true)
+            {
+                Debug.Log("LightSpotted!");
+                CurrentLight = LightSeeker.collider.GetComponentInParent<Transform>().transform;
+                anima.SetBool("isPatrol", false);
+                anima.SetBool("isLight", true);
+                
+                
+                
+
+            }
+            
+
+        }
+        else
+        {
+            Debug.DrawLine(transform.position + new Vector3(0, 1f, 0f), transform.position + transform.right * LightSeekingRange + new Vector3(0, 1f, 0f), Color.yellow);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Gizmos.DrawRay(transform.position, transform.right * detectRange);
+    }
+
+    private void OnDrawGizmos()
+    {
+
+
+        /*Vector3 VisionBox = new Vector2(6, 1) * 2;
+         Gizmos.DrawWireCube(transform.position + VisionBox / 2, VisionBox);*/
+       
+       
+        
+    }
+
+
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (player.GetComponent<Player>().hidden == false)
+            {
+
+
+                if (GameObject.Find("Hold Panel").transform.childCount != 0 && GameObject.Find("Hold Panel").transform.GetChild(0).GetComponent<Item_Inventory>().itemName == "talisman")
+                {
+                    if (anima.GetBool("isIdle") == false)
+                    {
+                        anima.SetBool("isPatrol", false);
+                        anima.SetBool("isChase", false);
+                        anima.SetBool("isIdle", true);
+
+                        this.gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+                        Destroy(GameObject.Find("Hold Panel").transform.GetChild(0).gameObject);
+                        StartCoroutine(EnemyWake(TalismanStunTime));
+
+                    }
+
+                }
+                else
+                {
+
+
+                    Destroy(GameObject.FindGameObjectWithTag("Player"));
+
+                    int child = GameObject.Find("DeathCanvas").transform.childCount;
+                    for (int i = 0; i < child; i++)
+                    {
+                        GameObject.Find("DeathCanvas").transform.GetChild(i).gameObject.SetActive(true);
+                    }
+
+
+                }
+            }
+
+        }
+      
+
+       
+
+
+
+
+    }
+    IEnumerator EnemyWake(int stuntime)
+    {
+        //This is a coroutine
+        yield return new WaitForSeconds(stuntime);    //Wait one frame
+        anima.SetBool("isPatrol", true);
+        anima.SetBool("isChase", false);
+        anima.SetBool("isIdle", false);
+
+        this.gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
+
+    }
+
+   
+}
---
+++ Assets/Scripts/Enemy AI/CarrotTest/CarrotPatrol.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotPatrol : StateMachineBehaviour
+{
+    public float speed;
+    public Transform[] moveSpots;
+    public Transform[] Set1;
+    private int randomspot;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        randomspot = 1;
+        moveSpots = animator.gameObject.GetComponent<CarrotMain>().moveSpots;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, moveSpots[randomspot].position, speed * Time.deltaTime);
+
+        if (moveSpots[randomspot].position.x < animator.transform.position.x && animator.transform.eulerAngles.y == 0)
+        {
+            animator.transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+        else if (moveSpots[randomspot].position.x > animator.transform.position.x && animator.transform.eulerAngles.y == 180)
+        {
+            animator.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Vector2.Distance(animator.transform.position, moveSpots[randomspot].position) < 0.2f)
+        {
+            randomspot = Random.Range(0, moveSpots.Length);
+
+        }
+    }
+
+
+   /* void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Candle"))
+        {
+            if (other.GetComponent<Candle>().isLit == true)
+            {
+
+
+                other.GetComponent<Candle>().isLit = false;
+
+                animator.SetBool("isPatrol", false);
+                animator.SetBool("isChase", false);
+                animator.SetBool("isIdle", true);
+
+                StartCoroutine(EnemyWake());
+
+                    
+                
+
+
+            }
+        }
+        
+        else
+        {
+            Debug.Log("nothing to snuff");
+        }
+
+        IEnumerator EnemyWake()
+        {
+            //This is a coroutine
+            yield return new WaitForSeconds(1);    //Wait one frame
+            GetComponent<SpriteRenderer>().sprite = spriteNotLit;
+            transform.GetChild(0).gameObject.SetActive(isLit);
+            GameObject.FindGameObjectWithTag("Enemy").GetComponent<MainGhost>().enabled = true;
+
+            GetComponent<SpriteRenderer>().sprite = spriteNotLit;
+            transform.GetChild(0).gameObject.SetActive(isLit);
+
+
+        }
+    }*/
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
