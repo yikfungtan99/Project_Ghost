944b1935f4518bec64b4a38cf480000d6e2c20d9
junchoo <jun.choo.y@gmail.com>
2020-03-11 19:08:34 +0800

    Implemented the art, sprites can change upon selection

---
+++ Assets/TextMesh Pro/Resources/Fonts & Materials/ButtonSpriteChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSpriteChange : MonoBehaviour
+{
+    public Sprite glowSprite;
+    public Sprite pressedSprite;
+    Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.gameObject.GetComponent<Button>();
+        SetSprite();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSprite()
+    {
+        if ((button != null) && (glowSprite != null) && (pressedSprite != null))
+        {
+            SpriteState spriteState = new SpriteState();
+            spriteState = button.spriteState;
+
+            button.spriteState = spriteState;
+        }
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+
+}
