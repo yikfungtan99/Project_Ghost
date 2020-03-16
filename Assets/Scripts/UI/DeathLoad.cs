389aea0205265f5dcaf50fc269fb85869baf7b3e
sandwich189 <46155245+sandwich189@users.noreply.github.com>
2020-03-17 00:17:35 +0800

    Polished Tutorial
    Tutorial End when ghost reach the door
    Added checkpoint for tutorial

--- Assets/Scripts/UI/DeathLoad.cs
+++ Assets/Scripts/UI/DeathLoad.cs
@@ -7,19 +7,65 @@ public class DeathLoad : MonoBehaviour
 {
 
     GameManager gm;
+    public bool forTutorial = false;
+    public Tutorial_Triggers resetTrigger;
+    public Tutorial_Triggers resetTrigger2;
+    public Tutorial_Candle tutorialCandle;
+    private Vector2 ghostInitPos;
 
     private void Start()
     {
         gm = GameManager.Instance;
+
+        ghostInitPos = gm.ghostMain.transform.position;
+
     }
 
+    [System.Obsolete]
     public void Restart()
     {
-        //Application.LoadLevel(Application.loadedLevel);
 
-        gm.playerObject.transform.position = gm.playerCheckpointPosition;
-        gm.playerObject.SetActive(true);
+        if (forTutorial)
+        {
+            //Application.LoadLevel(Application.loadedLevel);
 
+            gm.playerObject.transform.position = gm.playerCheckpointPosition;
+            gm.playerObject.SetActive(true);
+            gm.player.isDead = false;
+
+            gm.ghostMain.transform.position = ghostInitPos;
+            gm.ghostMain.SetActive(false);
+
+            if (resetTrigger)
+            {
+                resetTrigger.isEnabled = true;
+            }
+
+            if (resetTrigger2)
+            {
+                resetTrigger2.ready = false;
+            }
+
+            if (!tutorialCandle.isLit)
+            {
+
+                tutorialCandle.isLit = true;
+                tutorialCandle.GetComponent<SpriteRenderer>().sprite = tutorialCandle.spriteLit;
+                tutorialCandle.transform.GetChild(0).gameObject.SetActive(true);
+
+            }
+
+            gm.roomManager.GetComponent<Animator>().Play("Bridal");
+
+            transform.GetChild(0).gameObject.SetActive(false);
+            
+        }
+        else
+        {
+
+            Application.LoadLevel(Application.loadedLevel);
+
+        }
 
     }
 
