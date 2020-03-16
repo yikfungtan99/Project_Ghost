86ea6d51043cd51a2795c3250c340b6b5e6637a1
sandwich189 <46155245+sandwich189@users.noreply.github.com>
2020-03-16 18:44:14 +0800

    art implementation

--- Assets/Scripts/UI/mainMenu.cs
+++ Assets/Scripts/UI/mainMenu.cs
@@ -10,7 +10,7 @@ public class mainMenu : MonoBehaviour
     {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        //SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(1);
     }
 
     public void QuitButton()
