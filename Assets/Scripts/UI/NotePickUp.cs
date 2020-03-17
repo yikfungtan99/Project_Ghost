b7bb626bafe1c19ad9aea459f41fb4c32846d10f
jinzhihoi <56183521+jinzhihoi@users.noreply.github.com>
2020-03-17 22:18:21 +0800

    Notes

---
+++ Assets/Scripts/UI/NotePickUp.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotePickUp : Interactable
+{
+    
+    static int NoteCount = 0;
+    public static bool Note1PickedUp = false;
+    public static bool Note2PickedUp = false;
+    public static bool Note3PickedUp = false;
+    public static bool Note4PickedUp = false;
+    public static bool Note5PickedUp = false;
+    public static bool Note6PickedUp = false;
+    public static bool Note7PickedUp = false;
+    public static bool Note8PickedUp = false;
+    public override void Awake()
+    {
+        base.Awake();
+        NoteCount = 0;
+        Note1PickedUp = false;
+        Note2PickedUp = false;
+        Note3PickedUp = false;
+        Note4PickedUp = false;
+        Note5PickedUp = false;
+        Note6PickedUp = false;
+        Note7PickedUp = false;
+        Note8PickedUp = false;
+    }
+
+   
+
+    public override void Interact()
+    {
+            base.Interact();
+            NoteCount += 1;
+            Debug.Log(NoteCount);
+            Destroy(gameObject);
+            Debug.Log("Note picked up");
+
+            if (NoteCount == 1)
+            {
+                Note1PickedUp = true;
+                Debug.Log("Note1 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 2)
+            {
+                Note2PickedUp = true;
+                Debug.Log("Note2 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 3)
+            {
+                Note3PickedUp = true;
+                Debug.Log("Note3 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 4)
+            {
+                Note4PickedUp = true;
+                Debug.Log("Note4 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 5)
+            {
+                Note5PickedUp = true;
+                Debug.Log("Note5 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 6)
+            {
+                Note6PickedUp = true;
+                Debug.Log("Note6 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 7)
+            {
+                Note7PickedUp = true;
+                Debug.Log("Note7 has been picked up");
+                UpdateMonologue();
+            }
+            if (NoteCount == 8)
+            {
+                Note8PickedUp = true;
+                Debug.Log("Note8 has been picked up");
+                UpdateMonologue();
+            }
+    }
+   
+    void UpdateMonologue()
+    {
+        GameObject.Find("MonologueManager").GetComponent<MonologueManager>().DisplaySentence(3);
+    }
+}
--- Assets/Scripts/UI/PausingScript.cs
+++ Assets/Scripts/UI/PausingScript.cs
@@ -4,48 +4,44 @@ using UnityEngine;
 
 public class PausingScript : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
     public Canvas canvas;
     void Start()
     {
        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(GameIsPaused == true)
+        if (GameManager.Instance.gamePaused == true)
         {
-            canvas.transform.GetChild(2).gameObject.SetActive(true);
-            GameIsPaused = false;
+            GetCanvasChild(2).SetActive(true);
         }
+    }
+
+    GameObject GetCanvasChild(int index)
+    {
+        return canvas.transform.GetChild(index).gameObject;
     }
 
     public void ToPause()
     {
-        GameIsPaused = true;
+        Debug.Log("ToPause()");
         Pause();
     }
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
+        // pauseMenuUI.SetActive(false);
+        GetCanvasChild(2).SetActive(false);
         Time.timeScale = 1f;
-        GameIsPaused = false;
+        GameManager.Instance.SetPause(false);
     }
-
     void Pause()
     {
-        pauseMenuUI.SetActive(true);
+        Debug.Log("Pause()");
+        GetCanvasChild(2).SetActive(true);
         Time.timeScale = 0f;
-    }
-
-    public void LoadOptions()
-    {
-        Debug.Log("Loading options...");
-        canvas.transform.GetChild(2).gameObject.SetActive(false);
-        canvas.transform.GetChild(1).gameObject.SetActive(true);
+        GameManager.Instance.SetPause(true);
     }
 
     public void QuitGame()
@@ -53,11 +49,136 @@ public class PausingScript : MonoBehaviour
         Application.Quit();
     }
 
-    public void OptionsBack()
+    public void ToNotes()
     {
-        Debug.Log("Returning to menu...");
-        canvas.transform.GetChild(2).gameObject.SetActive(true);
-        canvas.transform.GetChild(1).gameObject.SetActive(false);
+        Debug.Log(NotePickUp.Note1PickedUp);
+        Debug.Log(NotePickUp.Note2PickedUp);
+        if (NotePickUp.Note1PickedUp == false && NotePickUp.Note2PickedUp == false)
+        {
+            GetCanvasChild(2).SetActive(false);
+            GetCanvasChild(3).SetActive(true);//NoNotesYetMenu
+            GetCanvasChild(4).SetActive(false);
+            GetCanvasChild(5).SetActive(false);
+        }
+        else if (NotePickUp.Note1PickedUp == true && NotePickUp.Note2PickedUp == false)
+        {
+            GetCanvasChild(2).SetActive(false);
+            GetCanvasChild(3).SetActive(false);
+            GetCanvasChild(4).SetActive(true);//NotesMenu1
+            GetCanvasChild(4).transform.GetChild(0).gameObject.SetActive(false);//NotesMenu1's next button
 
+            GameObject.Find("NotesMenu1").SetActive(true);
+            Debug.Log("Set Note1Active");
+        }
+        else if (NotePickUp.Note1PickedUp == true && NotePickUp.Note2PickedUp == true)
+        {
+            GetCanvasChild(2).SetActive(false);
+            GetCanvasChild(3).SetActive(false);
+            GetCanvasChild(4).SetActive(true);
+            GetCanvasChild(4).transform.GetChild(0).gameObject.SetActive(true);
+        }
     }
+
+    public void AllowedToGoToNextNote() //NOT USED (FOR NOW)
+    {
+        GetCanvasChild(2).SetActive(false);
+        GetCanvasChild(3).SetActive(false);
+        GetCanvasChild(4).SetActive(false);
+        GetCanvasChild(5).SetActive(false);
+    }
+
+    public void CanGoFromNote2ToNote3()
+    {
+        if(NotePickUp.Note3PickedUp == false)
+        {
+            GetCanvasChild(5).transform.GetChild(0).gameObject.SetActive(false);
+        }
+        if(NotePickUp.Note3PickedUp == true)
+        {
+            GetCanvasChild(5).transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void CanGoFromNote3ToNote4()
+    {
+        if (NotePickUp.Note4PickedUp == false)
+        {
+            GetCanvasChild(6).transform.GetChild(0).gameObject.SetActive(false);
+        }
+        if (NotePickUp.Note4PickedUp == true)
+        {
+            GetCanvasChild(6).transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void CanGoFromNote4ToNote5()
+    {
+        if (NotePickUp.Note5PickedUp == false)
+        {
+            GetCanvasChild(7).transform.GetChild(0).gameObject.SetActive(false);
+        }
+        if (NotePickUp.Note5PickedUp == true)
+        {
+            GetCanvasChild(7).transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void CanGoFromNote5ToNote6()
+    {
+        if (NotePickUp.Note6PickedUp == false)
+        {
+            GetCanvasChild(8).transform.GetChild(0).gameObject.SetActive(false);
+        }
+        if (NotePickUp.Note6PickedUp == true)
+        {
+            GetCanvasChild(8).transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void CanGoFromNote6ToNote7()
+    {
+        if (NotePickUp.Note7PickedUp == false)
+        {
+            GetCanvasChild(9).transform.GetChild(0).gameObject.SetActive(false);
+        }
+        if (NotePickUp.Note7PickedUp == true)
+        {
+            GetCanvasChild(9).transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void CanGoFromNote7ToNote8()
+    {
+        if (NotePickUp.Note8PickedUp == false)
+        {
+            GetCanvasChild(10).transform.GetChild(0).gameObject.SetActive(false);
+        }
+        if (NotePickUp.Note8PickedUp == true)
+        {
+            GetCanvasChild(10).transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    public void ReturnToPreviousNote() //NOT USED
+    {
+        GetCanvasChild(2).SetActive(false);
+        GetCanvasChild(3).SetActive(false);
+        GetCanvasChild(4).SetActive(true);
+        GetCanvasChild(5).SetActive(false);
+    }
+
+    public void NotesBack()
+    {
+        GetCanvasChild(2).SetActive(true);//PauseMenu
+        GetCanvasChild(3).SetActive(false);//NoNotesYetMenu
+        GetCanvasChild(4).SetActive(false);//NotesMenu1
+        GetCanvasChild(5).SetActive(false);//NotesMenu2
+        GetCanvasChild(6).SetActive(false);//NotesMenu3
+        GetCanvasChild(7).SetActive(false);//NotesMenu4
+        GetCanvasChild(8).SetActive(false);//NotesMenu5
+        GetCanvasChild(9).SetActive(false);//NotesMenu6
+        GetCanvasChild(10).SetActive(false);//NotesMenu7
+        GetCanvasChild(11).SetActive(false);//NotesMenu8
+    }
+
 }
