f234790f0333d09f37a02eece91e3e638b4993d6
shijie-l <59986979+shijie-l@users.noreply.github.com>
2020-04-14 16:18:54 +0800

    Test Scene Works, Added lore scripts, Tidied up script folder

---
+++ Assets/Scripts/Special Effects, Events/Events/Lore Interactables/LivingRoomLore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingRoomLore : Interactable
+{
+    [Header("Living Room Lore")]
+    [SerializeField] bool isGrandfatherClock = false;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        IdentifyObject();
+    }
+
+    void IdentifyObject()
+    {
+        if(isGrandfatherClock)
+        {
+            UpdateMonologue(1);
+        }
+    }
+
+    void UpdateMonologue(int displayIndex)
+    {
+        switch(displayIndex)
+        {
+            case 1: //! Grandfather Clock
+                gm.monologueManager.DisplaySentence(40);
+                break;
+            default:
+                break;
+        }
+    }
+}
