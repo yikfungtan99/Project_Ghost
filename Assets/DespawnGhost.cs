96ea3c65959c4f16afc7ea253f0139b8eea60d4a
jinzhihoi <56183521+jinzhihoi@users.noreply.github.com>
2020-04-19 20:10:31 +0800

    Main Scene tutorial
    Candle has bug .

---
+++ Assets/DespawnGhost.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnGhost : MonoBehaviour
+{
+    GameManager gm;
+    private void Start()
+    {
+        gm = GameManager.Instance;
+
+        
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gm.ghostMain.GetComponent<Transform>().position = gm.carrotMain.inactiveLocation.position;
+
+    }
+}
